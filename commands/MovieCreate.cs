4e391183b234f9f518e8f4fe93893e9ef99734ab
0996736Ilias <0996736@hr.nl>
2020-03-18 12:15:08 +0100

    create movie database and movies

---
+++ commands/MovieCreate.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using Project.Base;
+using Project.Enums;
+using Project.Helpers;
+using Project.Models;
+using Project.Services;
+
+
+namespace Project.Commands {
+
+    class MovieCreate : InteractiveCommand {
+
+        public override string GetCategory() {
+            return "film";
+        }
+
+        public override bool RequireAdmin() {
+            return true;
+        }
+
+        public override string GetName() {
+            return "create";
+        }
+
+        public override void RunCommand(string[] args) {
+            Program app = Program.GetInstance();
+            MovieManager movieManager = app.GetService<MovieManager>("movies");
+            Movie currentMvoie = movieManager.GetCurrentMovie();
+
+            // Get input
+            string movieName = AskQuestion("Wat is uw film naam?");
+            string minage = AskQuestion("leeftijdsclassificatie?");
+            string time = AskQuestion("hoe lang duurt de film?");
+            
+            // Try to registers
+            Movie movie = new movie(movieName, minage, time);
+
+            // Login if registration successful
+            if (movieManager.SaveMovie(movie)) {
+                movieManager.SetCurrentMovie(movie);
+                ConsoleHelper.Print(PrintType.Info, "Film succesvol aangemaakt.");
+            }
+            else {
+                ConsoleHelper.Print(PrintType.Info, "Kon film niet aanmaken. Errors:");
+
+                // Print errors
+                foreach (KeyValuePair<string, List<string>> attribute in movies.GetErrors()) {
+                    foreach (string error in attribute.Value) {
+                        ConsoleHelper.Print(PrintType.Error, attribute.Key + " -> " + error);
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
