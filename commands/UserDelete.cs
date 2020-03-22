8694c91af6fb919d2f65d120a251d1d2f7d8b53c
Tim van Dijkhuizen <timvandijkhuizen@hotmail.nl>
2020-03-22 01:14:00 +0100

    Command, database & console changes

---
+++ commands/UserDelete.cs
@@ -0,0 +1,51 @@
+﻿using Project.Base;
+using Project.Enums;
+using Project.Helpers;
+using Project.Models;
+using Project.Services;
+using System;
+
+namespace Project.Commands {
+
+    class UserDelete : Command {
+
+        public override string GetCategory() {
+            return "user";
+        }
+
+        public override string GetName() {
+            return "delete";
+        }
+
+        public override bool RequireAdmin() {
+            return true;
+        }
+
+        public override void RunCommand(string[] args) {
+            Program app = Program.GetInstance();
+            UserManager userManager = app.GetService<UserManager>("users");
+
+            // Check args length
+            if (args.Length != 1) {
+                ConsoleHelper.Print(PrintType.Error, "Usage: user/delete <id>");
+                return;
+            }
+
+            // Find room
+            int id = ConsoleHelper.ParseInt(args[0], "id");
+            User user = userManager.GetUserById(id);
+
+            if (user == null) {
+                throw new ArgumentException("id moet een bestaand gebruiker-id zijn.");
+            }
+
+            if (userManager.DeleteUser(user)) {
+                ConsoleHelper.Print(PrintType.Info, "Gebruiker succesvol verwijderd.");
+            } else {
+                ConsoleHelper.Print(PrintType.Error, "Kon gebruiker niet verwijderen.");
+            }
+        }
+
+    }
+
+}
--- helpers/ConsoleHelper.cs
+++ helpers/ConsoleHelper.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Project.Base;
 using Project.Enums;
@@ -23,6 +24,40 @@ namespace Project.Helpers {
                 foreach (string error in attribute.Value) {
                     Print(PrintType.Error, attribute.Key + " -> " + error);
                 }
+            }
+        }
+
+        // Parses an int value
+        public static int ParseInt(string value, string paramName = null) {
+            try {
+                return Int32.Parse(value);
+            } catch(FormatException) {
+                throw new ArgumentException((paramName ?? value) + " moet een integer zijn.");
+            }
+        }
+
+        // Parses an double value
+        public static double ParseDouble(string value, string paramName = null) {
+            try {
+                return Double.Parse(value);
+            } catch (FormatException) {
+                throw new ArgumentException((paramName ?? value) + " moet een double zijn.");
+            }
+        }
+
+        // Parses an boolean value
+        public static bool ParseBoolean(string value, string paramName = null) {
+            switch(value) {
+                case "ja":
+                case "yes":
+                case "true":
+                    return true;
+                case "nee":
+                case "no":
+                case "false":
+                    return false;
+                default:
+                    throw new ArgumentException((paramName ?? value) + " moet een boolean zijn.");
             }
         }
 
