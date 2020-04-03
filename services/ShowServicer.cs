4fe87dc6fcc93d4e069cd1fc166daddeeff6d5b7
gioksz <57941142+gioksz@users.noreply.github.com>
2020-04-03 14:50:53 +0200

    werk

--- services/ShowServicer.cs
+++ services/ShowServicer.cs
@@ -20,76 +20,77 @@ namespace Project.Services {
             return "shows";
         }
 
-        public override void Load() {
-            database = new ShowDatabase();
-
-            ConsoleHelper.Print(PrintType.Info, "Loading show database...");
-
-            // Try to load
-            if (!database.Load()) {
-                ConsoleHelper.Print(PrintType.Error, "Failed to load shows");
-                return;
-            }
-
-            ConsoleHelper.Print(PrintType.Info, "Loaded show database.");
-
-
-        }
-
-        public override void Unload() {
-            ConsoleHelper.Print(PrintType.Info, "Saving user database...");
-
-            // Try to save
-            if (!database.Save()) {
-                ConsoleHelper.Print(PrintType.Error, "Failed to save user database.");
-                return;
-            }
-
-            ConsoleHelper.Print(PrintType.Info, "Saved user database.");
-        }
-
         // Returns all shows
         public List<Show> GetShows() {
             List<Show> models = new List<Show>();
 
-            foreach(ShowRecord record in database.shows){
+            foreach (ShowRecord record in database.shows) {
                 models.Add(new Show(record));
             }
 
             return models;
         }
 
-        
+        // Returns a show by its id
+        public Show GetRoomById(int id) {
+            Database database = Program.GetInstance().GetDatabase();
 
-        // Saves the specified user
-        public bool SaveShow(Show show) {
-            bool isNew = show.id == -1;
-
-            // Set id if its a new user
-            if (isNew) {
-                show.id = database.GetNewId("shows");
+            try {
+                return new Show(database.shows.Where(i => i.id == id).First());
             }
+            catch (InvalidOperationException) {
+                return null;
+            }
+        }
 
+            // Validate and add if valid
+            if (!show.Validate()) {
+            return false;
+        }
+
+            // Set id if its a new room
+            if (isNew) {
+            show.id = database.GetNewId("shows");
+                          }
 
             // Find existing record
-            ShowRecord record = database.shows.SingleOrDefault(i => i.id == show.id);
-            
+            ShowRecord record = database.rooms.SingleOrDefault(i => i.id == show.id);
+
             // Add if no record exists
-            if(record == null) {
+            if (record == null) {
                 record = new ShowRecord();
                 database.shows.Add(record);
-            }
+             }
+
 
             // Update record
-            record.id = show.id;
-            record.Movie = show.Movie;
-            record.Date = show.Date;
-            record.Room = show.Room;
-            record.Time = show.Time;
-  
+            record.id = room.id;
+            record.number = room.number;
+
+            // Try to save
+            database.TryToSave();
 
             return true;
-        }
-    }
+            }
 
+            // Deletes the specified room
+            public bool DeleteShow(Show show) {
+            Program app = Program.GetInstance();
+            Database database = app.GetDatabase();
+            ShowRecord record = database.shows.SingleOrDefault(i => i.id == show.id);
+
+            // Return false if room doesn't exist
+            if (record == null) {
+                 return false;
+            }
+
+            // Remove record and chairs
+            database.shows.Remove(record);
+
+            // Try to save
+             database.TryToSave();
+
+            return true;
 }
+
+
