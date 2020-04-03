659fb11cae6d67ecff0ae687d6e4ab30710c4786
gioksz <57941142+gioksz@users.noreply.github.com>
2020-04-03 13:50:41 +0200

    Update ShowDelete.cs
    Update show delete tot try to get it working

--- commands/ShowDelete.cs
+++ commands/ShowDelete.cs
@@ -14,41 +14,95 @@ namespace Project.Commands {
             return "show";
         }
 
-        public override bool RequireLogin() {
-            return true;
+        public override string GetName() {
+            return "delete";
         }
 
-        public override string GetName() {
-            return "create";
+        public override bool RequireAdmin() {
+            return true;
         }
 
         public override void RunCommand(string[] args) {
             Program app = Program.GetInstance();
-            ShowManager showManager = app.GetService<ShowManager>("shows");
+            ShowManager showmanager = app.GetService<ShowManager>("chairs");
 
+            // Check args length
+            if (args.Length != 1) {
+                throw new ArgumentException("Gebruik: " + GetUsage());
+            }
 
-            // Get input
-            string Movie = AskQuestion("wat is de naam van de film");
-            string Room = AskQuestion("in welke room speelt de film");
-            string Date = AskQuestion("wat is de datum van de film");
-            string Time = AskQuestion("welke tijd wordt de film afgespeelt");
+            // Find room
+            int id = ConsoleHelper.ParseInt(args[0], "id");
+            Show show= ShowManager.GetChairById(id);
 
-            
-            
+            if (show == null) {
+                throw new ArgumentException("Ongeldige stoel");
+            }
 
-
-
-            // Try to register
-            Show show = new Show(Movie, Room, Date, Time);
-
-            // Login if registration successful
-            if (showManager.SaveShow(show)) {
-                ConsoleHelper.Print(PrintType.Info, "show succesvol aangemaakt.");
+            if (chairService.DeleteChair(chair)) {
+                ConsoleHelper.Print(PrintType.Info, "Stoel succesvol verwijderd");
             }
             else {
-                ConsoleHelper.Print(PrintType.Info, "Kon show niet aanmaken. Errors:");
+                ConsoleHelper.Print(PrintType.Error, "Kon stoel niet verwijderen");
             }
         }
+
+    }
+
+
+}
+using Project.Base;
+using Project.Enums;
+using Project.Helpers;
+using Project.Models;
+using Project.Services;
+using System;
+
+namespace Project.Commands {
+
+    class ChairDelete : Command {
+
+        public override string GetCategory() {
+            return "chair";
+        }
+
+        public override string GetName() {
+            return "delete";
+        }
+
+        public override string GetUsage() {
+            return "chair/delete <id>";
+        }
+
+        public override bool RequireAdmin() {
+            return true;
+        }
+
+        public override void RunCommand(string[] args) {
+            Program app = Program.GetInstance();
+            ChairService chairService = app.GetService<ChairService>("chairs");
+
+            // Check args length
+            if (args.Length != 1) {
+                throw new ArgumentException("Gebruik: " + GetUsage());
+            }
+
+            // Find room
+            int id = ConsoleHelper.ParseInt(args[0], "id");
+            Chair chair = chairService.GetChairById(id);
+
+            if (chair == null) {
+                throw new ArgumentException("Ongeldige stoel");
+            }
+
+            if (chairService.DeleteChair(chair)) {
+                ConsoleHelper.Print(PrintType.Info, "Stoel succesvol verwijderd");
+            }
+            else {
+                ConsoleHelper.Print(PrintType.Error, "Kon stoel niet verwijderen");
+            }
+        }
+
     }
 
 }
