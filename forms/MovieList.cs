7f7686f193f32425a833f297ebe693759c39f925
Tim van Dijkhuizen <timvandijkhuizen@hotmail.nl>
2020-05-21 22:33:21 +0200

    Text changes

--- forms/MovieList.cs
+++ forms/MovieList.cs
@@ -45,7 +45,7 @@ namespace Project.Forms {
 
         private void MovieList_Load(object sender, System.EventArgs e) {
             container.View = View.Details;
-            container.Columns.Add("Player", 250);
+            container.Columns.Add("Films", 250);
         }
 
         private void Populate() {
