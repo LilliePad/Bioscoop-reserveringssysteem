2702f1b4837ca13a9e0651ed5fc0a191d1244f5e
gioksz <57941142+gioksz@users.noreply.github.com>
2020-05-21 22:02:28 +0200

    renamed form1 to movieCreate

---
+++ forms/MovieCreate.cs
@@ -0,0 +1,14 @@
+﻿using Project.Forms.Layouts;
+using Project.Base;
+using Project.Enums;
+using Project.Helpers;
+using Project.Models;
+using Project.Services;
+
+namespace Project.Forms {
+    public partial class MovieCreate : BaseLayout {
+        public Form1() {
+            InitializeComponent();
+        }
+    }
+}
