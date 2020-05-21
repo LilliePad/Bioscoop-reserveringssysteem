980454cf6d27c4d5c839e641ecaa6374080d69bc
Tim van Dijkhuizen <timvandijkhuizen@hotmail.nl>
2020-05-21 11:58:11 +0200

    Cleanup & BaseLayout

--- forms/MovieList.cs
+++ forms/MovieList.cs
@@ -1,245 +1,27 @@
-﻿using System.Windows.Forms;
-using System;
-using Project.Models;
+﻿namespace Project.Forms {
 
-namespace Project.Forms {
-
-    public partial class MovieList : Form {
-        private Panel panel1;
-        private Panel panel2;
-        private Button button6;
-        private Button button5;
-        private Button button4;
-        private Button button3;
-        private Button button2;
-        private Button button1;
-        private System.ComponentModel.IContainer components;
-        private CheckBox checkBox1;
-        private GroupBox groupBox1;
-        private HScrollBar hScrollBar1;
-        private TextBox textBox2;
-        private TextBox textBox1;
-        private int number = 0;
-        private string username;
-        private string password;
+    public partial class MovieList : BaseLayout {
 
         public MovieList() {
             InitializeComponent();
         }
 
         private void InitializeComponent() {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.panel2.SuspendLayout();
-            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.BackColor = System.Drawing.Color.Black;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1258, 0);
-            this.panel1.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel2.BackColor = System.Drawing.Color.Black;
-            this.panel2.Controls.Add(this.textBox2);
-            this.panel2.Controls.Add(this.textBox1);
-            this.panel2.Controls.Add(this.button6);
-            this.panel2.Controls.Add(this.button5);
-            this.panel2.Controls.Add(this.button4);
-            this.panel2.Controls.Add(this.button3);
-            this.panel2.Controls.Add(this.button2);
-            this.panel2.Controls.Add(this.button1);
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1280, 100);
-            this.panel2.TabIndex = 1;
-            // 
-            // button6
-            // 
-            this.button6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button6.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.button6.BackColor = System.Drawing.Color.White;
-            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button6.Location = new System.Drawing.Point(1151, 32);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(99, 30);
-            this.button6.TabIndex = 5;
-            this.button6.Text = "Login";
-            this.button6.UseVisualStyleBackColor = false;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
-            // 
-            // button5
-            // 
-            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button5.Location = new System.Drawing.Point(320, 55);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(110, 30);
-            this.button5.TabIndex = 4;
-            this.button5.UseVisualStyleBackColor = false;
-            // 
-            // button4
-            // 
-            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Location = new System.Drawing.Point(460, 55);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(110, 30);
-            this.button4.TabIndex = 3;
-            this.button4.UseVisualStyleBackColor = false;
-            // 
-            // button3
-            // 
-            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Location = new System.Drawing.Point(600, 55);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(110, 30);
-            this.button3.TabIndex = 2;
-            this.button3.UseVisualStyleBackColor = false;
-            // 
-            // button2
-            // 
-            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(180, 55);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(110, 30);
-            this.button2.TabIndex = 1;
-            this.button2.UseVisualStyleBackColor = false;
-            // 
-            // button1
-            // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(40, 55);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(110, 30);
-            this.button1.TabIndex = 0;
-            this.button1.UseVisualStyleBackColor = false;
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(0, 0);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
-            this.checkBox1.TabIndex = 2;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.hScrollBar1);
-            this.groupBox1.Location = new System.Drawing.Point(90, 166);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1098, 391);
-            this.groupBox1.TabIndex = 3;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
-            // 
-            // hScrollBar1
-            // 
-            this.hScrollBar1.Location = new System.Drawing.Point(758, 367);
-            this.hScrollBar1.Name = "hScrollBar1";
-            this.hScrollBar1.Size = new System.Drawing.Size(248, 21);
-            this.hScrollBar1.TabIndex = 0;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(1035, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 7;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(1035, 38);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.PasswordChar = '*';
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 8;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+
             // 
             // MovieList
             // 
             this.ClientSize = new System.Drawing.Size(1262, 673);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
             this.Name = "MovieList";
             this.Load += new System.EventHandler(this.MovieList_Load);
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
-            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
-
-        }
-
-        private void label1_Click(object sender, System.EventArgs e) {
-            number += 1;
-           
         }
 
         private void MovieList_Load(object sender, System.EventArgs e) {
 
         }
 
-        private void label1_Click_1(object sender, System.EventArgs e) {
-
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e) {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e) {
-            username = textBox1.Text;
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e) {
-            password = textBox2.Text;
-        }
-
-        private void button6_Click(object sender, EventArgs e) {
-
-        }
     }
 
 }
---
+++ forms/layouts/BaseLayout.cs
@@ -0,0 +1,182 @@
+﻿using System.Windows.Forms;
+using System;
+using Project.Models;
+
+namespace Project.Forms {
+
+    public partial class BaseLayout : Form {
+
+        private Panel navBar;
+
+        private Button navLink1;
+        private Button navLink2;
+        private Button navLink3;
+        private Button navLink4;
+        private Button navLink5;
+
+        private TextBox navLoginUsername;
+        private TextBox navLoginPassword;
+        private Button navLoginButton;
+
+        private string usernameValue;
+        private string passwordValue;
+
+        public BaseLayout() {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent() {
+            this.navBar = new System.Windows.Forms.Panel();
+            this.navLink1 = new System.Windows.Forms.Button();
+            this.navLink2 = new System.Windows.Forms.Button();
+            this.navLink3 = new System.Windows.Forms.Button();
+            this.navLink4 = new System.Windows.Forms.Button();
+            this.navLink5 = new System.Windows.Forms.Button();
+            this.navLoginUsername = new System.Windows.Forms.TextBox();
+            this.navLoginPassword = new System.Windows.Forms.TextBox();
+            this.navLoginButton = new System.Windows.Forms.Button();
+            this.navBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // navBar
+            // 
+            this.navBar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.navBar.BackColor = System.Drawing.Color.Black;
+            this.navBar.Controls.Add(this.navLink1);
+            this.navBar.Controls.Add(this.navLink2);
+            this.navBar.Controls.Add(this.navLink3);
+            this.navBar.Controls.Add(this.navLink4);
+            this.navBar.Controls.Add(this.navLink5);
+            this.navBar.Controls.Add(this.navLoginUsername);
+            this.navBar.Controls.Add(this.navLoginPassword);
+            this.navBar.Controls.Add(this.navLoginButton);
+            this.navBar.Location = new System.Drawing.Point(0, 0);
+            this.navBar.Name = "navBar";
+            this.navBar.Size = new System.Drawing.Size(1261, 100);
+            this.navBar.TabIndex = 1;
+            // 
+            // navLink1
+            // 
+            this.navLink1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.navLink1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.navLink1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.navLink1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.navLink1.Location = new System.Drawing.Point(40, 55);
+            this.navLink1.Name = "navLink1";
+            this.navLink1.Size = new System.Drawing.Size(110, 30);
+            this.navLink1.TabIndex = 0;
+            this.navLink1.UseVisualStyleBackColor = false;
+            // 
+            // navLink2
+            // 
+            this.navLink2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.navLink2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.navLink2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.navLink2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.navLink2.Location = new System.Drawing.Point(180, 55);
+            this.navLink2.Name = "navLink2";
+            this.navLink2.Size = new System.Drawing.Size(110, 30);
+            this.navLink2.TabIndex = 1;
+            this.navLink2.UseVisualStyleBackColor = false;
+            // 
+            // navLink3
+            // 
+            this.navLink3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.navLink3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.navLink3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.navLink3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.navLink3.Location = new System.Drawing.Point(320, 55);
+            this.navLink3.Name = "navLink3";
+            this.navLink3.Size = new System.Drawing.Size(110, 30);
+            this.navLink3.TabIndex = 4;
+            this.navLink3.UseVisualStyleBackColor = false;
+            // 
+            // navLink4
+            // 
+            this.navLink4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.navLink4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.navLink4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.navLink4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.navLink4.Location = new System.Drawing.Point(460, 55);
+            this.navLink4.Name = "navLink4";
+            this.navLink4.Size = new System.Drawing.Size(110, 30);
+            this.navLink4.TabIndex = 3;
+            this.navLink4.UseVisualStyleBackColor = false;
+            // 
+            // navLink5
+            // 
+            this.navLink5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.navLink5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.navLink5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.navLink5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.navLink5.Location = new System.Drawing.Point(600, 55);
+            this.navLink5.Name = "navLink5";
+            this.navLink5.Size = new System.Drawing.Size(110, 30);
+            this.navLink5.TabIndex = 2;
+            this.navLink5.UseVisualStyleBackColor = false;
+            // 
+            // navLoginUsername
+            // 
+            this.navLoginUsername.Location = new System.Drawing.Point(1035, 12);
+            this.navLoginUsername.Name = "navLoginUsername";
+            this.navLoginUsername.Size = new System.Drawing.Size(100, 29);
+            this.navLoginUsername.TabIndex = 7;
+            this.navLoginUsername.TextChanged += new System.EventHandler(this.NavLoginUsername_TextChanged);
+            // 
+            // navLoginPassword
+            // 
+            this.navLoginPassword.Location = new System.Drawing.Point(1035, 38);
+            this.navLoginPassword.Name = "navLoginPassword";
+            this.navLoginPassword.PasswordChar = '*';
+            this.navLoginPassword.Size = new System.Drawing.Size(100, 29);
+            this.navLoginPassword.TabIndex = 8;
+            this.navLoginPassword.TextChanged += new System.EventHandler(this.NavLoginPassword_TextChanged);
+            // 
+            // navLoginButton
+            // 
+            this.navLoginButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.navLoginButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.navLoginButton.BackColor = System.Drawing.Color.White;
+            this.navLoginButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.navLoginButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.navLoginButton.Location = new System.Drawing.Point(1151, 37);
+            this.navLoginButton.Name = "navLoginButton";
+            this.navLoginButton.Size = new System.Drawing.Size(99, 30);
+            this.navLoginButton.TabIndex = 5;
+            this.navLoginButton.Text = "Login";
+            this.navLoginButton.UseVisualStyleBackColor = false;
+            this.navLoginButton.Click += new System.EventHandler(this.NavLoginButton_Click);
+            // 
+            // BaseLayout
+            // 
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.navBar);
+            this.Name = "BaseLayout";
+            this.Load += new System.EventHandler(this.MovieList_Load);
+            this.navBar.ResumeLayout(false);
+            this.navBar.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        private void MovieList_Load(object sender, System.EventArgs e) {
+
+        }
+
+        private void NavLoginUsername_TextChanged(object sender, EventArgs e) {
+            usernameValue = navLoginUsername.Text;
+
+        }
+
+        private void NavLoginPassword_TextChanged(object sender, EventArgs e) {
+            passwordValue = navLoginPassword.Text;
+        }
+
+        private void NavLoginButton_Click(object sender, EventArgs e) {
+
+        }
+    }
+
+}
