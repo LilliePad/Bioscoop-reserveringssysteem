e4790c991b8b8ce577bb8b7073fb6c4923b6f711
gioksz <57941142+gioksz@users.noreply.github.com>
2020-05-22 22:17:43 +0200

    Revert "Styling"
    This reverts commit f304092c21a40f20ded18928303d4a8e6c11dcc4.

--- forms/MovieList.cs
+++ forms/MovieList.cs
@@ -45,7 +45,7 @@ namespace Project.Forms {
 
         private void MovieList_Load(object sender, System.EventArgs e) {
             container.View = View.Details;
-            container.Columns.Add("Films", 600);
+            container.Columns.Add("Films", 250);
         }
 
         private void Populate() {
