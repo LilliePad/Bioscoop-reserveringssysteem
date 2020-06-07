5e28383e7748373a074a0b4e1b69f6798eeecd48
Lillie <57945088+LilliePad@users.noreply.github.com>
2020-06-07 15:10:49 +0200

    Started work on ReservatioDetail UwU

---
+++ forms/ReservationDetail.cs
@@ -0,0 +1,17 @@
+﻿using Project.Forms.Layouts;
+using System.Windows.Forms;
+using System;
+using Project.Base;
+using Project.Models;
+using Project.Services;
+using Project.Helpers;
+using Project;
+using Project.Forms;
+
+namespace Bioscoop_reserveringssysteem.forms {
+    public partial class ReservationDetail : BaseLayout {
+        public ReservationDetail() {
+            InitializeComponent();
+        }
+    }
+}
